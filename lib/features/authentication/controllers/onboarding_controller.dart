a0c786efe8b9a17ccdf36728f59290f309430cd9
jima05 <jimabenjamin4@gmail.com>
2023-12-28 14:00:16 +0300

    ON Boarding Fully done

--- lib/features/authentication/controllers/onboarding_controller.dart
+++ lib/features/authentication/controllers/onboarding_controller.dart
@@ -1,19 +1,35 @@
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-class onBoardingController extends GetxController {
-  static onBoardingController get instance => Get.find();
+class OnBoardingController extends GetxController {
+  static OnBoardingController get instance => Get.find();
 
   // Variables
+  final pageController = PageController();
+  Rx<int> currentPageIndex = 0.obs;
 
   // Update current Index when page scrolls
-  void updatePageIndicator(index) {}
+  void updatePageIndicator(index) => currentPageIndex.value = index;
 
   // Jump to the specific dot selected page
-  void dotNavigationClick(index) {}
+  void dotNavigationClick(index) {
+    currentPageIndex.value = index;
+    pageController.jumpTo(index);
+  }
 
   // Update current Index & jump to next page
-  void nextPage() {}
+  void nextPage() {
+    if (currentPageIndex.value == 2) {
+      // Get.to(LoginScreen());
+    } else {
+      int page = currentPageIndex.value + 1;
+      pageController.jumpToPage(page);
+    }
+  }
 
   // Update current Index & jump to last page
-  void skipPage() {}
+  void skipPage() {
+    currentPageIndex.value = 2;
+    pageController.jumpToPage(2);
+  }
 }
