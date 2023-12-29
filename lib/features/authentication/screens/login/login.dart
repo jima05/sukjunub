a97f61c2758bfdbae1656722e2ec4230e671fe30
jima05 <jimabenjamin4@gmail.com>
2023-12-29 15:59:01 +0300

    Email, Password, Remember me & Forget password

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:sukjunub/common/styles/spacing_styles.dart';
 import 'package:sukjunub/utils/constants/image_strings.dart';
 import 'package:sukjunub/utils/constants/sizes.dart';
@@ -24,13 +25,72 @@ class LoginScreen extends StatelessWidget {
                 children: [
                   Image(
                     height: 150,
-                    image: AssetImage( dark ? SukjunubImages.lightAppLogo : SukjunubImages.darkAppLogo),
+                    image: AssetImage(dark
+                        ? SukjunubImages.lightAppLogo
+                        : SukjunubImages.darkAppLogo),
                   ),
-                  Text(SukjunubTexts.loginTitle, style: Theme.of(context).textTheme.headlineMedium,),
-                  const SizedBox(height: SukjunubSizes.sm,),
-                  Text(SukjunubTexts.loginSubTitle, style: Theme.of(context).textTheme.bodyMedium,),
+                  Text(
+                    SukjunubTexts.loginTitle,
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
+                  const SizedBox(
+                    height: SukjunubSizes.sm,
+                  ),
+                  Text(
+                    SukjunubTexts.loginSubTitle,
+                    style: Theme.of(context).textTheme.bodyMedium,
+                  ),
                 ],
-              )
+              ),
+
+              // Form
+              Form(
+                child: Column(
+                  children: [
+                    // Email
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Iconsax.direct_right),
+                          labelText: SukjunubTexts.email),
+                    ),
+
+                    const SizedBox(height: SukjunubSizes.spaceBtwInputFields),
+
+                    // Password
+                    TextFormField(
+                      decoration: const InputDecoration(
+                        prefixIcon: Icon(Iconsax.password_check),
+                        labelText: SukjunubTexts.password,
+                        suffixIcon: Icon(Iconsax.eye_slash),
+                      ),
+                    ),
+
+                    const SizedBox(
+                        height: SukjunubSizes.spaceBtwInputFields / 2),
+
+                    // Remember Me  & Forget Password
+                    Row(
+                      children: [
+                        // Remember Me
+                        Row(
+                          children: [
+                            Checkbox(value: true, onChanged: (value) {}),
+                            const Text(SukjunubTexts.rememberMe),
+                          ],
+                        ),
+
+                        // Forget Password
+                        TextButton(
+                          onPressed: () {},
+                          child: const Text(SukjunubTexts.forgetPassword),
+                        ),
+                      ],
+                    ),
+
+                    const SizedBox(height: SukjunubSizes.spaceBtwSections),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
