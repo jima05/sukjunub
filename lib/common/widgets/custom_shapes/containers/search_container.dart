a2617432beffb828fcfa1fd31710cedbab1cd67a
jima05 <jimabenjamin4@gmail.com>
2024-01-03 09:26:31 +0300

    SearchBar complete

---
+++ lib/common/widgets/custom_shapes/containers/search_container.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:sukjunub/utils/constants/colors.dart';
+import 'package:sukjunub/utils/constants/sizes.dart';
+import 'package:sukjunub/utils/device/device_utility.dart';
+import 'package:sukjunub/utils/helpers/helper_functions.dart';
+
+class SukjunubSearchContainer extends StatelessWidget {
+  const SukjunubSearchContainer({
+    super.key,
+    required this.text,
+    this.icon = Iconsax.search_normal,
+    this.showBackground = true,
+    this.showBorder = true,
+  });
+
+  final String text;
+  final IconData? icon;
+  final bool showBackground, showBorder;
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = SukjunubHelperFunctions.isDarkMode(context);
+    return Padding(
+      padding:
+          const EdgeInsets.symmetric(horizontal: SukjunubSizes.defaultSpace),
+      child: Container(
+        width: SukjunubDeviceUtils.getScreenWidth(context),
+        padding: const EdgeInsets.all(SukjunubSizes.md),
+        decoration: BoxDecoration(
+          color: showBackground
+              ? dark
+                  ? SukjunubColors.dark
+                  : SukjunubColors.light
+              : Colors.transparent,
+          borderRadius: BorderRadius.circular(SukjunubSizes.cardRadiusLg),
+          border: showBorder ? Border.all(color: SukjunubColors.grey) : null,
+        ),
+        child: Row(children: [
+          Icon(icon, color: SukjunubColors.darkerGrey),
+          const SizedBox(width: SukjunubSizes.spaceBtwItems),
+          Text(text, style: Theme.of(context).textTheme.bodySmall),
+        ]),
+      ),
+    );
+  }
+}
--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 import 'package:sukjunub/common/widgets/custom_shapes/containers/primary_header_container.dart';
+import 'package:sukjunub/common/widgets/custom_shapes/containers/search_container.dart';
 import 'package:sukjunub/features/shop/screens/home/widgets/home_appbar.dart';
+import 'package:sukjunub/utils/constants/colors.dart';
+import 'package:sukjunub/utils/constants/sizes.dart';
+import 'package:sukjunub/utils/device/device_utility.dart';
+import 'package:sukjunub/utils/helpers/helper_functions.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -13,7 +19,14 @@ class HomeScreen extends StatelessWidget {
           children: [
             SukjunubPrimaryHeaderContainer(
                 child: Column(
-              children: [SukjunubHomeAppBar()],
+              children: [
+                // AppBar
+                SukjunubHomeAppBar(),
+                SizedBox(height: SukjunubSizes.spaceBtwSections),
+                // SearchBar
+                SukjunubSearchContainer(text: 'Search in Store',),
+                // Categories
+              ],
             )),
           ],
         ),
@@ -21,3 +34,4 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
+
